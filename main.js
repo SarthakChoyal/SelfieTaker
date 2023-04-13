c1c240c7b3781c38c7f5a1109fcf30d29f733fdd
Sarthak Choyal <94727252+SarthakChoyal@users.noreply.github.com>
2023-04-13 10:59:24 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -15,15 +15,32 @@ recognition.onresult = function(event) {
     console.log(Content);
 
     document.getElementById("textbox").innerHTML = Content;
-        speak()
+    console.log(Content);
+    if(Content =="take my selfie")
+    {
+        console.log("taking selfie ---");
+        speak();
+    }
 }
 
 function speak(){
     var synth = window.speechSynthesis;
-    speak_data = document.getElementById("textbox").value;
+    speak_data = "Taking your selfie in 5 seconds";
     var utterThis = new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterThis);
     Webcam.attach(camera);
+
+    setTimeout(function()
+    {
+        take_snapshot();
+        save();
+    }, 5500);
+}
+function save(){
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src ;
+    link.href = image;
+    link.click();
 }
 
 Webcam.set({
@@ -33,4 +50,13 @@ Webcam.set({
     png_quality:90
 });
 camera = document.getElementById("camera");
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'">';
+
+    });
+    
+}
 
